5de76afcdf9f05bb052c88fa0154e70f2010fff6
Omri Ashkenazi <omri.ashke@gmail.com>
2025-06-29 00:21:51 +0100

    enable auto formatting

--- neovim/lua/omriashkenazi/remap.lua
+++ neovim/lua/omriashkenazi/remap.lua
@@ -40,9 +40,23 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local bufnr = ev.buf
-        local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  callback = function(ev)
+    local bufnr = ev.buf
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then return end
+
+    ---@diagnostic disable-next-line: missing-parameter
+    if client.supports_method('textDocument/formatting') then
+      -- Format the current buffer on save
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id })
+        end
+      })
     end
+  end
 })
