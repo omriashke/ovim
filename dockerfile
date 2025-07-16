b07e9b2b0131e466bc47e47f3e9d78a265c03f38
Omri Ashkenazi <omri.ashke@gmail.com>
2025-07-16 19:42:35 +0300

    for now remve tmux resurrect

--- dockerfile
+++ dockerfile
@@ -20,6 +20,8 @@ COPY ./neovim/init.lua /root/.config/nvim/init.lua
 
 RUN nvim --headless -c "lua require('omriashkenazi.packer')" -c "autocmd User PackerComplete quitall" -c "PackerSync"
 
+RUN nvim --headless +"TSInstall rust javascript typescript html css python c lua bash" +qa
+
 COPY ./neovim/after /root/.config/nvim/after
 
 # dependecies
--- neovim/after/plugin/treesitter.lua
+++ neovim/after/plugin/treesitter.lua
@@ -1,7 +1,6 @@
-require'nvim-treesitter'.setup {
+require 'nvim-treesitter'.setup {
   -- Directory to install parsers and queries to
   install_dir = vim.fn.stdpath('data') .. '/site'
 }
-require'nvim-treesitter'.install { 'rust', 'javascript', 'typescript', 'cpp', 'html', 'css', 'python', 'c', 'lua', 'bash' }
 
-require'treesitter-context'.setup()
+require 'treesitter-context'.setup()
